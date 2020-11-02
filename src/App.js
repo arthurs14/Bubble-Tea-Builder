aed598c09f95bb9809c0bdde3507e18fb8f29516
Arthur Sandro <arthurcs426@gmail.com>
2020-11-01 16:21:36 -0800

    added open sans, also used Higher Order Component to wrap our layout component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-function App() {
-  return (
-    <div>
+import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
 
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <p>Test</p>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
