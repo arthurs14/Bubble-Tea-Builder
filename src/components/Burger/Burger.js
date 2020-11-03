4debf270b5e7bf104db749a7bb0112c5ef377c33
Arthur Sandro <arthurcs426@gmail.com>
2020-11-02 16:50:23 -0800

    reduce the array of ingredients to a cleaner array and added a conditional to tell you if you to add ingredients if there is none

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,15 +1,21 @@
+import { array } from 'prop-types';
 import React from 'react';
 import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredients/BurgerIngredient';
 
 const burger = ({ ingredients }) => {
   // turns object into an array by keys then created BurgerIngredient for every
-  const transformedIngredients = Object.keys(ingredients).map(igKey => {
+  let transformedIngredients = Object.keys(ingredients).map(igKey => {
     return [...Array(ingredients[igKey])].map((_, i) => {
-      console.log(igKey, i);
       return <BurgerIngredient key={igKey + i} type={igKey} />
     });
-  });
+  }).reduce((arr, el) => {
+    return arr.concat(el);
+  }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Start adding ingredients!</p>;
+  }
 
   return (
     <div className={classes.Burger}>
