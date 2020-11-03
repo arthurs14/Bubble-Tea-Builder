4bbf195ae5145d924ba58cf6f66d8041b0df228b
Arthur Sandro <arthurcs426@gmail.com>
2020-11-02 16:38:53 -0800

    use state to hold a starting amount of ingredients and dynamically show em without hard coding them in

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,12 +2,19 @@ import React from 'react';
 import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredients/BurgerIngredient';
 
-const burger = (props) => {
+const burger = ({ ingredients }) => {
+  // turns object into an array by keys then created BurgerIngredient for every
+  const transformedIngredients = Object.keys(ingredients).map(igKey => {
+    return [...Array(ingredients[igKey])].map((_, i) => {
+      console.log(igKey, i);
+      return <BurgerIngredient key={igKey + i} type={igKey} />
+    });
+  });
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
