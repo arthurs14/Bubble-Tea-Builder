0232bd2ba8df5de5ef910f15c6f9466e1100bc0d
Arthur Sandro <arthurcs426@gmail.com>
2020-11-10 17:21:19 -0800

    just changed file structure a bit to reorganize the files

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import Aux from '../../../hoc/Auxiliary';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
+  // This could be a functional component, doesn't have to be a class
   componentDidUpdate() {
     console.log('[OrderSummary ] Did Update');
   }
