b5771fa09f3a671bb8f31ceab62454098a977a9c
Arthur Sandro <arthurcs426@gmail.com>
2020-11-10 17:07:12 -0800

    improved performance on the modal and ordersummary from reupdating everytime we added a new ingredient - it should only now update when we click the button

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,32 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Auxiliary';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = ({ ingredients, purchaseCancelled, purchaseContinued, total }) => {
-  const ingredientSummary = Object.keys(ingredients).map(igKey => {
+class OrderSummary extends Component {
+  componentDidUpdate() {
+    console.log('[OrderSummary ] Did Update');
+  }
+
+  render() {
+    const { ingredients, purchaseCancelled, purchaseContinued, total } = this.props;
+    const ingredientSummary = Object.keys(ingredients).map(igKey => {
+      return (
+        <li key={igKey + 1}>
+          <span
+            style={{ textTransform: 'capitalize' }}>
+            {igKey}
+          </span>: {ingredients[igKey]}
+        </li>
+      );
+    });
+
     return (
-      <li key={igKey + 1}>
-        <span
-          style={{ textTransform: 'capitalize' }}>
-          {igKey}
-        </span>: {ingredients[igKey]}
-      </li>
+      <Aux>
+        <h3>Your Order</h3>
+        <p>Burger with the follow:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total Price</strong>: ${total.toFixed(2)}</p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={purchaseCancelled}>CANCEL</Button>
+        <Button btnType="Success" clicked={purchaseContinued}>CONTINUE</Button>
+      </Aux>
     );
-  });
+  }
+}
 
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>Burger with the follow:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total Price</strong>: ${total.toFixed(2)}</p>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={purchaseCancelled}>CANCEL</Button>
-      <Button btnType="Success" clicked={purchaseContinued}>CONTINUE</Button>
-    </Aux>
-  );
-};
-
-export default orderSummary;
+export default OrderSummary;
