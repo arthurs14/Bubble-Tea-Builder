39f8fd7a9e5fde1547ff2578dc421350677cc832
Arthur Sandro <arthurcs426@gmail.com>
2020-11-18 16:17:19 -0800

    added a loading animation when sending our order to firebase; just so there is some visual queue to show that its being added

--- src/containers/BurgerBuider/BurgerBuilder.js
+++ src/containers/BurgerBuider/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import Spinner from '../../components/UI/Spinner/Spinner';
 import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
@@ -24,6 +25,7 @@ class BurgerBuilder extends Component {
     totalPrice: 4,
     purchaseable: false,
     purchasing: false,
+    loading: false,
   };
 
   updatePurchaseState(ingredients) {
@@ -87,6 +89,9 @@ class BurgerBuilder extends Component {
     //alert('You continued!');
     const { ingredients, totalPrice } = this.state;
 
+    // shows loading animation to show that its processing
+    this.setState({ loading: true });
+
     const order = {
       ingredients: ingredients,
       price: totalPrice,
@@ -102,12 +107,17 @@ class BurgerBuilder extends Component {
       deliveryMethod: 'fastest',
     }
     axios.post('/orders.json', order)
-      .then(response => console.log(response))
-      .catch(error => console.log(error));
+      .then(response => {
+        // set it back to false so that it will go back to the order summary
+        this.setState({ loading: false, purchasing: false });
+      })
+      .catch(error => {
+        this.setState({ loading: false, purchasing: false });
+      });
   }
 
   render() {
-    const { ingredients, totalPrice, purchaseable, purchasing } = this.state;
+    const { ingredients, totalPrice, purchaseable, purchasing, loading } = this.state;
     let disabledInfo = {
       ...ingredients
     };
@@ -116,14 +126,20 @@ class BurgerBuilder extends Component {
       disabledInfo[key] = disabledInfo[key] <= 0
     }
 
+    let orderSummary = <OrderSummary
+      ingredients={ingredients}
+      purchaseCancelled={this.purchaseCancelHandler}
+      purchaseContinued={this.purchaseContinueHandler}
+      total={totalPrice} />;
+
+    if (loading) {
+      orderSummary = <Spinner />
+    }
+
     return (
       <Aux>
         <Modal show={purchasing} modalClosed={this.purchaseCancelHandler}>
-          <OrderSummary
-            ingredients={ingredients}
-            purchaseCancelled={this.purchaseCancelHandler}
-            purchaseContinued={this.purchaseContinueHandler}
-            total={totalPrice} />
+          {orderSummary}
         </Modal>
         <Burger ingredients={ingredients} />
         <BuildControls
