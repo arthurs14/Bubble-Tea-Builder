13982fc13f2305d536ca0ca25e476dab15be9e5f
Arthur Sandro <arthurcs426@gmail.com>
2020-12-01 16:51:47 -0800

    added ordery summary and order checkout files

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+
+
+const checkoutSummary = (props) => {
+  return (
+    <div>
+      <h1>Enjoy Your Burger!</h1>
+      <div style={{ width: '300px', height: '300px', margin: 'auto' }}>
+        <Burger ingredients={props.ingredients} />
+      </div>
+      <Button btnType="Danger">Cancel</Button>
+      <Button btnType="Success">Continue</Button>
+    </div>
+  );
+};
+
+export default checkoutSummary;
---
+++ src/containers/Checkout/Checkout.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+// Summary of what the user will buy
+class Checkout extends Component {
+}
+
+export default Checkout;
