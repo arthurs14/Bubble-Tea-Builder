93dfb6a1f9e2368049b6b45a431c00f50fe005a4
Arthur Sandro <arthurcs426@gmail.com>
2020-12-01 16:59:33 -0800

    completed a dummy burger to show as an order summary below the main burger builder for now

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -2,17 +2,19 @@ import React from 'react';
 
 import Burger from '../../Burger/Burger';
 import Button from '../../UI/Button/Button';
-
+import classes from './CheckoutSummary.module.css';
 
 const checkoutSummary = (props) => {
   return (
-    <div>
+    <div className={classes.CheckoutSummary}>
       <h1>Enjoy Your Burger!</h1>
-      <div style={{ width: '300px', height: '300px', margin: 'auto' }}>
+      <div style={{ width: '100%', margin: 'auto' }}>
         <Burger ingredients={props.ingredients} />
       </div>
-      <Button btnType="Danger">Cancel</Button>
-      <Button btnType="Success">Continue</Button>
+      <Button
+        btnType="Danger">Cancel</Button>
+      <Button
+        btnType="Success">Continue</Button>
     </div>
   );
 };
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
 
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+
 // Summary of what the user will buy
 class Checkout extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      meat: 1,
+      cheese: 1,
+      bacon: 1,
+    },
+  };
+
+  render() {
+    const { ingredients } = this.state;
+    return (
+      <div>
+        <CheckoutSummary ingredients={ingredients} />
+      </div>
+    );
+  }
 }
 
 export default Checkout;
