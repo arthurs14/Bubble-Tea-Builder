d42ef805f32ea24a16efb280e28f3be87406f925
Arthur Sandro <arthurcs426@gmail.com>
2020-12-02 18:13:14 -0800

    added styling for the form and made it show when you click continue on the order summary page

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
 
 // Summary of what the user will buy
 class Checkout extends Component {
@@ -41,6 +44,8 @@ class Checkout extends Component {
           ingredients={ingredients}
           checkoutCancelled={this.checkoutCancelledHandler}
           checkoutContinued={this.checkoutContinuedHandler} />
+
+        <Route path={this.props.match.path + '/contact-data'} component={ContactData} />
       </div>
     );
   }
