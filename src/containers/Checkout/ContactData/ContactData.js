8d8f03eb647a2289bdd21e21e781e17f267fa50e
Arthur Sandro <arthurcs426@gmail.com>
2020-12-06 16:10:32 -0800

    added a spinner after submitting an order to indicate that it is sending

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Button from '../../../components/UI/Button//Button';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.module.css';
 import axios from '../../../axios-orders';
 
@@ -51,16 +52,25 @@ class ContactData extends Component {
   }
 
   render() {
+    const { loading } = this.state;
+    let form = (
+      <form>
+        <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
+        <input className={classes.Input} type="email" name="email" placeholder="Your Email" />
+        <input className={classes.Input} type="text" name="street" placeholder="Street" />
+        <input className={classes.Input} type="text" name="postal" placeholder="Postal" />
+        <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+      </form>
+    );
+
+    if (loading) {
+      form = <Spinner />;
+    }
+
     return (
       <div className={classes.ContactData}>
         <h4>Enter Your Contact Data</h4>
-        <form>
-          <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
-          <input className={classes.Input} type="email" name="email" placeholder="Your Email" />
-          <input className={classes.Input} type="text" name="street" placeholder="Street" />
-          <input className={classes.Input} type="text" name="postal" placeholder="Postal" />
-          <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
-        </form>
+        {form}
       </div>
     );
   }
