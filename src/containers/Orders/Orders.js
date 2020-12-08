ebbedf31142508b7b7810bf4adefcfd26609acb6
Arthur Sandro <arthurcs426@gmail.com>
2020-12-07 17:06:01 -0800

    creating an order page

---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+
+class Orders extends Component {
+  render() {
+    return ();
+  }
+}
+
+export default Orders;
