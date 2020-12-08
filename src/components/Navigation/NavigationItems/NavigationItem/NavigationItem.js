7192a753fe0cf76467977860f97e9fd2b0af421a
Arthur Sandro <arthurcs426@gmail.com>
2020-12-07 17:22:27 -0800

    made some changes from checkout to listing all existing orders

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom'
+
 import classes from './NavigationItem.module.css';
 
 const navigationItem = ({ item, link, active }) => (
   <li className={classes.NavigationItem}>
-    <a
-      href={link}
-      className={active ? classes.active : null}>{item}</a>
+    <NavLink
+      to={link}
+      exact
+      activeClassName={classes.active}>{item}</NavLink>
   </li>
 );
 
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,8 +4,8 @@ import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = (props) => (
   <ul className={classes.NavigationItems}>
-    <NavigationItem item="Burger Builder" link="/" active />
-    <NavigationItem item="Checkout" link="/" />
+    <NavigationItem item="Burger Builder" link="/" />
+    <NavigationItem item="Orders" link="/orders" />
   </ul>
 );
 
