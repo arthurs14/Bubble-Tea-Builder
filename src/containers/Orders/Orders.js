9d72caa1f3326a9f071808716084e842eb74b862
Arthur Sandro <arthurcs426@gmail.com>
2020-12-07 17:11:11 -0800

    added some fake data to show on our orders page

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
-import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+
+import Order from '../../components/Order/Order';
 
 class Orders extends Component {
   render() {
-    return ();
+    return (
+      <div>
+        <Order />
+        <Order />
+      </div>
+    );
   }
 }
 
