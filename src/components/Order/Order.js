042975d4bf533c21cfe3b8a6f177830a74d54a7e
Arthur Sandro <arthurcs426@gmail.com>
2020-12-08 18:03:28 -0800

    full output of ingredients and price on orders page

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,11 +2,31 @@ import React from 'react';
 
 import classes from './Order.module.css';
 
-const order = ({ ingredients, price }) => (
-  <div className={classes.Order}>
-    <p>Ingredients: Salad(1)</p>
-    <p>Price: <strong>USD {price.toFixed(2)}</strong></p>
-  </div>
-);
+const order = ({ ingredients, price }) => {
+  let extractedIngredients = [];
+
+  for (let ingredientName in ingredients) {
+    extractedIngredients.push({ name: ingredientName, amount: ingredients[ingredientName] });
+  }
+
+  const ingredientOutput = extractedIngredients.map(ig => {
+    return <span
+      style={{
+        textTransform: 'capitalize',
+        display: 'inline-block',
+        margin: '0 8px',
+        border: '1px solid #ccc',
+        padding: '5px',
+      }}
+      key={ig.name}>{`${ig.name} (${ig.amount})`}</span>;
+  })
+
+  return (
+    <div className={classes.Order}>
+      <p>Ingredients: {ingredientOutput}</p>
+      <p>Price: <strong>USD {price.toFixed(2)}</strong></p>
+    </div>
+  );
+};
 
 export default order;
