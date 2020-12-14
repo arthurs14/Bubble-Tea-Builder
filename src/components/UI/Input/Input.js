55f7add5ec9f93f90c86c203ac13196460933199
Arthur Sandro <arthurcs426@gmail.com>
2020-12-13 16:51:38 -0800

    added select options to the form

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -5,7 +5,7 @@ import classes from './Input.module.css';
 const input = (props) => {
   let inputElement = null;
 
-  switch (props.elelmentType) {
+  switch (props.elementType) {
     case ('input'):
       inputElement = <input
         className={classes.InputElement}
@@ -17,6 +17,17 @@ const input = (props) => {
         className={classes.InputElement}
         {...props.elementConfig}
         value={props.value} />;
+      break;
+    case ('select'):
+      inputElement = (
+        <select
+          className={classes.InputElement}
+          value={props.value}>
+          {props.elementConfig.options.map(option => (
+            <option value={option.value}>{option.displayValue}</option>
+          ))}
+        </select>
+      );
       break;
     default:
       inputElement = <input
