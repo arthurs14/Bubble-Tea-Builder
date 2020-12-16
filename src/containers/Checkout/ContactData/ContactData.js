821aa94b822f73cf021e36ce4de3e8f085ad6e04
Arthur Sandro <arthurcs426@gmail.com>
2020-12-16 14:51:50 -0800

    conditional was written wrong

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -42,6 +42,8 @@ class ContactData extends Component {
         value: '',
         validation: {
           required: true,
+          minLength: 5,
+          maxLength: 5,
         },
         valid: false,
       },
@@ -121,6 +123,14 @@ class ContactData extends Component {
 
     if (rules.required) {
       isValid = value.trim() !== '';
+    }
+
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength;
+    }
+
+    if (rules.maxLength) {
+      isValid = value.length >= rules.maxLength;
     }
 
     return isValid;
