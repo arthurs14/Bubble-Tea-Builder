0987191237e6d460b9963d546be3cd1507145466
Arthur Sandro <arthurcs426@gmail.com>
2021-01-07 18:14:48 -0800

    made add ingredient working

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,14 +1,25 @@
 import * as actionTypes from './actions';
 
 const initialState = {
-  ingredients: null,
+  ingredients: {
+    salad: 0,
+    bacon: 0,
+    cheese: 0,
+    meat: 0,
+  },
   price: 4,
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.ADD_INGREDIENTS:
-      return state;
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] + 1,
+        }
+      };
     case actionTypes.REMOVE_INGREDIENTS:
       return state;
     default:
