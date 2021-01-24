3456b2bb3c93cdac2177aa3186c3e16019886ce2
Arthur Sandro <arthurcs426@gmail.com>
2021-01-24 14:42:36 -0800

    fixed an issue if refreshing from the contact-page to go back to burger builder page

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
@@ -17,18 +17,23 @@ class Checkout extends Component {
 
   render() {
     const { ings } = this.props;
-    return (
-      <div>
-        <CheckoutSummary
-          ingredients={ings}
-          checkoutCancelled={this.checkoutCancelledHandler}
-          checkoutContinued={this.checkoutContinuedHandler} />
+    let summary = <Redirect to="/" />
 
-        <Route
-          path={this.props.match.path + '/contact-data'}
-          component={ContactData} />
-      </div>
-    );
+    if (ings) {
+      summary = 
+        <div>
+          <CheckoutSummary
+            ingredients={ings}
+            checkoutCancelled={this.checkoutCancelledHandler}
+            checkoutContinued={this.checkoutContinuedHandler} />
+          
+          <Route
+            path={this.props.match.path + '/contact-data'}
+            component={ContactData} />
+        </div>;
+    }
+
+    return summary;
   }
 }
 
