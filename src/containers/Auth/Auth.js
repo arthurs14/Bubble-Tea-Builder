a7ccb6258dc1c8c0941674d0253e9038447b373d
Arthur Sandro <arthurcs426@gmail.com>
2021-02-07 15:39:00 -0800

    added another parameter to be taken into our onAuth function to further assign a token for users

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -89,7 +89,7 @@ class Auth extends Component {
 
   submitHandler = (ev) => {
     ev.preventDefault();
-    this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+    this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignup);
   }
 
   switchAuthModeHandler = () => {
@@ -138,7 +138,7 @@ class Auth extends Component {
 
 const mapDispatchToProps = dispatch => {
   return {
-    onAuth: (email, password) => dispatch(actions.auth(email, password)),
+    onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup)),
   };
 };
 
