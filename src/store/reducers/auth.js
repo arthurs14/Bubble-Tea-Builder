7332dae170f8e665d45b31955d06a3271e598ba1
Arthur Sandro <arthurcs426@gmail.com>
2021-02-08 17:39:56 -0800

    added a new reducer file for auth with reducer and initial state

---
+++ src/store/reducers/auth.js
@@ -0,0 +1,24 @@
+import * as actionTypes from '../actions/actionTypes';
+import updateObject from '../utility';
+
+let initialState = {
+  token: null,
+  userId: null,
+  error: null,
+  loading: false,
+}
+
+const authStart = (state, action) => {
+  return updateObject(state, { error: null, loading: true });
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.AUTH_START:
+      return authStart(state, action);
+    default:
+      return state;
+  }
+};
+
+export default reducer;
