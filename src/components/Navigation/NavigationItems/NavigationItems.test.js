c18f774202d64dda4f772abb9baa54b30cc736da
Arthur Sandro <arthurcs426@gmail.com>
2021-03-01 15:54:37 -0800

    moved test file to navigationItems folder and added the required imports

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -1,13 +1,17 @@
-import { configure } from 'enzyme';
+import React from 'react';
+
+import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+import NavigationItems from './NavigationItems';
+
 // connects enzyme
-configure({adapter: new Adapter()});
+configure({ adapter: new Adapter() });
 
 // 1. description of test bundle (identify the kind of test)
 // 2. a function that will do a test
 describe('<NavigationItems />', () => {
   it('should render two <NavigationItem /> elements if not authenticated', () => {
-
+    const wrapper = shallow(<NavigationItems />);
   });
 });
