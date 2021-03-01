fb671535a6ca4f37a0b5def808d0d2e0259461cd
Arthur Sandro <arthurcs426@gmail.com>
2021-03-01 15:51:20 -0800

    configured enzyme to be connected to component

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItems.test.js
@@ -1,7 +1,13 @@
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+// connects enzyme
+configure({adapter: new Adapter()});
+
 // 1. description of test bundle (identify the kind of test)
 // 2. a function that will do a test
 describe('<NavigationItems />', () => {
   it('should render two <NavigationItem /> elements if not authenticated', () => {
-    
+
   });
 });
