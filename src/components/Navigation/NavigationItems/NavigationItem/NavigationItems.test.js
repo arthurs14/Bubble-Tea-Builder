3aec0d15c337dcaf1455443da114c9f1bbee7cb4
Arthur Sandro <arthurcs426@gmail.com>
2021-03-01 15:50:02 -0800

    added describe and it functions for testing NavigationItem component

---
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItems.test.js
@@ -0,0 +1,7 @@
+// 1. description of test bundle (identify the kind of test)
+// 2. a function that will do a test
+describe('<NavigationItems />', () => {
+  it('should render two <NavigationItem /> elements if not authenticated', () => {
+    
+  });
+});
