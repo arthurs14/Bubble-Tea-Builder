56cadddf5201fa3103228307a9d22dc3b4597872
Arthur Sandro <arthurcs426@gmail.com>
2021-03-07 17:11:05 -0800

    created a test to see that the initial state has the right properties

--- src/store/reducers/auth.test.js
+++ src/store/reducers/auth.test.js
@@ -1,2 +1,15 @@
 import reducer from './auth';
 import * as actionTypes from '../actions/actionTypes';
+
+describe('auth reducer', () => {
+  // How to test reducers by putting the initial state if none is given
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual({
+      token: null,
+      userId: null,
+      error: null,
+      loading: false,
+      authRedirectPath: '/',
+    });
+  });
+});
