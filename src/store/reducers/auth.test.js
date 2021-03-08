ec333611ed960417ce857e2a82f7740943cc5205
Arthur Sandro <arthurcs426@gmail.com>
2021-03-07 17:23:13 -0800

    made another test to test logout is working

--- src/store/reducers/auth.test.js
+++ src/store/reducers/auth.test.js
@@ -2,9 +2,20 @@ import reducer from './auth';
 import * as actionTypes from '../actions/actionTypes';
 
 describe('auth reducer', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = reducer({
+      token: null,
+      userId: null,
+      error: null,
+      loading: false,
+      authRedirectPath: '/',
+    }, {});
+  });
+
   // How to test reducers by putting the initial state if none is given
   it('should return the initial state', () => {
-    expect(reducer(undefined, {})).toEqual({
+    expect(wrapper).toEqual({
       token: null,
       userId: null,
       error: null,
