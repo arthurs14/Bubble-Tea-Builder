ba3bf348067dd723b52f5b368805528be87f64ec
Arthur <arthurcs426@gmail.com>
2021-03-23 14:58:49 -0700

    destructured action for cleaner code

--- src/store/sagas/orderSagas.js
+++ src/store/sagas/orderSagas.js
@@ -4,11 +4,12 @@ import axios from '../../axios-orders';
 import * as actions from '../actions';
 
 export function* purchaseBurgerSaga (action) {
+    const { token, orderData } = action;
     yield put(actions.purchaseBurgerStart());
 
     try {
-        const response = yield axios.post(`/orders.json?auth=${action.token}`, action.orderData);
-        yield put(actions.purchaseBurgerSuccess(response.data.name, action.orderData));
+        const response = yield axios.post(`/orders.json?auth=${token}`, orderData);
+        yield put(actions.purchaseBurgerSuccess(response.data.name, orderData));
     } catch (error) {
         yield put(actions.purchaseBurgerFail(error));
     }
