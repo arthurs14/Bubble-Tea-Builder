e1b9c3254bda2c1fd16720d786c3d657a3dd4b86
Arthur <arthurcs426@gmail.com>
2021-03-24 16:45:29 -0700

    use the word all to run all takeEvery instead

--- src/store/sagas/rootSagas.js
+++ src/store/sagas/rootSagas.js
@@ -1,4 +1,4 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, all } from 'redux-saga/effects';
 
 import * as actionTypes from '../actions/actionTypes';
 import { 
@@ -12,10 +12,12 @@ import { purchaseBurgerSaga, fetchOrdersSaga } from './orderSagas';
 
 // Generator
 export function* watchAuth () {
-    yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
-    yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
-    yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
-    yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga);
+    yield all([
+        takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga),
+        takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga),
+        takeEvery(actionTypes.AUTH_USER, authUserSaga),
+        takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga),
+    ]);
 }
 
 export function* watchBurgerBuilder () {
